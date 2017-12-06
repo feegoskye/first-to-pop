d0207c519455c381e612e26813090a6f4095cbbf
Orlando Grant <orlandogrant@Orlandos-MacBook-Pro.local>
2017-12-06 02:42:32 -0500

    just finished the calculator, no bugs pure perfection

--- javascript/sample.js
+++ javascript/sample.js
@@ -1,3 +1,27 @@
 var textfield1 = document.getElementById('tetxtfield1');
 var textfield2 = document.getElementById('textfield2');
-var gabber = document.getElementById('grabber');
+var grabber = document.getElementById('grabber');
+var result = document.getElementById('result');
+
+grabber.addEventListener('submit',function(event){
+    
+    if (!textfield1.value || !textfield2){
+      alert("please enter something in the fields"); 
+    
+    }
+    else{
+        var x = parseFloat(textfield1.value);
+        var y = parseFloat(textfield2.value);
+        
+        //algorithm
+        var answer = x / y;
+        var percent = answer * 100;
+        
+        result.innerText = "Result: " +percent + "%";
+        
+        event.preventDefault();
+        
+    }
+        
+    
+});
